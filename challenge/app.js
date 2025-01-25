0cb5289d64e60fae8598d67ea8488478f80f7550
Diego <diegoubatuba103@gmail.com>
2025-01-25 10:46:53 -0300

    adicionando função sortear amigo

--- challenge/app.js
+++ challenge/app.js
@@ -7,8 +7,41 @@ function adicionarAmigo(){
     if (amigo.value == ''){
         alert ('Por favor, insira um nome.');
     }else {
-        amigos.push(amigo.value);
-        console.log(amigos);
-        amigo.value ='';
+        if(amigos.includes(amigo.value)){
+            alert('Este nome já foi adicionado.');
+        }else{
+            amigos.push(amigo.value);
+            amigo.value = '';
+        }
     }
+    mostrarLista();
+}
+function mostrarLista (){
+    let lista = document.querySelector('.name-list');
+    lista.innerHTML = '';
+    for (let i = 0; i< amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+function limparLista(){
+    amigos = [];
+    mostrarLista();
+    let lista = document.querySelector('.name-list');
+    lista.innerHTML = '';
+    
+}
+
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert('por favor, adicione um amigo antes de sortear');
+    }else{
+        let sorteado = Math.floor(Math.random()*amigos.length);
+        let amigoSorteado = amigos[sorteado];
+        let resultado = document.querySelector('.result-list');
+        resultado.innerHTML = '';
+        resultado.innerHTML = `O amigo secreto sorteado é:<li>${amigoSorteado}</li>`;
+        amigos
+        
+    }
+    limparLista();
 }
